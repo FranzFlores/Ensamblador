f20092d22f8e1e0592df66baa0d3e5288d400ab7
Franz Flores <andresfloresgallardo@gmail.com>
2019-12-04 22:20:23 -0500

    Ejecutor
    Archivo que permite la ejecución de un archivo de ensamblador

---
+++ ejecutor.sh
@@ -0,0 +1,11 @@
+//Demonio usado para mediante un comando ejecutar las instrucciones de un archivo de ensamblador
+
+if [ $# -eq 1 ]
+then
+	echo "Compilando y ejecutando...."
+	nasm -f elf $1.asm; ld -m elf_i386 -s -o $1 $1.o
+	./$1
+else
+	echo "..............."
+	echo "Error"
+fi
